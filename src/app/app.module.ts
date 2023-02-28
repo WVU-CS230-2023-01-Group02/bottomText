2ca2f1e5845cbc5bef1d9de839ac05d227af433e
adsarver <andsarve15@gmail.com>
2023-02-28 11:17:21 -0500

    Started homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { BackgroundComponent } from './home/background/background.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BackgroundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
