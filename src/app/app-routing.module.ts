2c7ce0087269ae570fe7a452aa387959c39323a7
Brandon Hines <bah00025@mix.wvu.edu>
2023-03-03 15:30:22 -0500

    Created Routing for Signup/Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './Layouts/login/login.component';
+import { SignupComponent } from './Layouts/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'signup',
+  component: SignupComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
