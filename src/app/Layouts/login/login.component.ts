8a34c0b6f02b5b106de4511c101a33ec3e7eab06
Brandon Hines <bah00025@mix.wvu.edu>
2023-03-06 11:15:59 -0500

    Daily Commit 1

--- src/app/Layouts/login/login.component.ts
+++ src/app/Layouts/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'layout-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'home-background',
+  selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.css']
 })
