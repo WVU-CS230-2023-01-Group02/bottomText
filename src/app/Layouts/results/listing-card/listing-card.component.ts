94d9ff5995f152c702c2dc2d19bbc8849f34f16a
HunterCriss <hwc00002@mix.wvu.edu>
2023-03-07 13:10:32 -0500

    Updating to format of Results Panes

--- src/app/Layouts/results/listing-card/listing-card.component.ts
+++ src/app/Layouts/results/listing-card/listing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-listing-card',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./listing-card.component.css']
 })
 export class ListingCardComponent {
+@Input() college: string;
+@Input() className: string;
+@Input() instructor: string;
+@Input() accountName: string;
+@Input() semester: string;
 
+constructor(){
+  this.college = "Undefined parameter";
+  this.className = "Undefined parameter";
+  this.instructor = "Undefined parameter";
+  this.accountName = "Undefined parameter";
+  this.semester = "Undefined parameter";
+}
 }
